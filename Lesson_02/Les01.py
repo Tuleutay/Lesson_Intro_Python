49acd8865660b80f38b3f3faafa89be5b0b99f5a
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-12-23 00:32:49 +0600

    Les02-com2

--- Lesson_02/Les01.py
+++ Lesson_02/Les01.py
@@ -3,16 +3,31 @@
 # w  открытие для записи данных
 # w+, r+
 
-colors = [ 'red', 'green', 'blue']
-data = open('file.txt', 'a')
-data.writelines(colors) # разделителей не будет
-data.close()
+# with open('file.txt', 'w') as data:
+#     data.write('line 111\n')
+#     data.write('line 1222\n')
 
 
-exit()
-path = 'file.txt'
-data = open(path, 'r')
-for line in data:
-    print(line)
-data.close()
+# colors = [ 'red', 'green', 'blue123']
+# data = open('file.txt', 'a')
+# data.writelines(colors) # разделителей не будет
+# data.write('LINE 121\n')
+# data.write('LINE 131\n')
+# data.close()
 
+
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+#
+# exit()
+def f(x):
+    if x ==1:
+        return 'Целое'
+    elif x==2.3:
+        return '23'
+    else:
+        return
---
+++ Lesson_02/Функции.py
@@ -0,0 +1,122 @@
+# import Les01 as h
+#
+# print(h.f(1))
+
+# def new_string(symbol, count=3):
+#     return symbol * count
+#
+# print(new_string('!', 5)) # !!!!!
+# print(new_string('!'))    # !!!
+# print(new_string(4))      # 12
+
+# def concatenatio(*params):
+#     res: str = ""
+#     for item in params:
+#         res += item
+#     return res
+#
+# print(concatenatio('a', 'b', 'i', 'k', 'e'))    # abike
+# print(concatenatio('a', '1'))                   # a1
+
+# def concatenatioint(*params):
+#     res: int = 0
+#     for item in params:
+#         res += item
+#     return res
+#
+# print(concatenatioint(1, 2, 3, 45))   # 51
+#
+#
+# ########## RECURSION
+#
+# def fib(n):
+#     if n in [1,2]:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+#
+# list = []
+# for e in range (1,10):
+#     list.append((fib(e)))
+# print(list)                 # [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+
+##############  К О Р Т Е Ж И - это неизменяемые списки
+
+# a = (3, 1, 41, 4)
+# # print(a)        # (3, 1, 41, 4)
+# # print(a[0])     # 3
+# # print(a[-2])    # 41
+# for item in a:
+#     print(item)
+
+####### СЛОВАРИ - это неупорядоченные коллекциии
+# произвольных объектов с доступом по ключу
+
+# dictionary = {}
+# dictionary = \
+#     {
+#         'up': '↑',
+#         'left': '←',
+#         'down': '↓',
+#         'right': '→'
+#     }
+# print(dictionary)           # {'up': '↑', 'left': '←', 'down': '↓', 'right': '→'}
+# print(dictionary['left'])   # ←
+# # типы ключей могут отличаться
+#
+# del dictionary['left']        # даление элемента
+# for v in dictionary.values():
+#     print(dictionary[v])
+# for item in dictionary:
+#   print('{}: {}'.format(item, dictionary[item]))
+#         up: ↑,
+#         left: ←,
+#         down: ↓,
+#         right: →
+
+###########  МНОЖЕСТВА
+
+# colors = {'black', 'green', 'blue123'}
+# print(colors)                               #{'green', 'black', 'blue123'}
+# colors.add('red')
+# print(colors)                               # {'black', 'green', 'red', 'blue123'}
+# colors.add('grey')
+# print(colors)                               # {'black', 'grey', 'red', 'green', 'blue123'}
+# colors.remove('red')
+# print(colors)                               # {'blue123', 'grey', 'black', 'green'}
+# colors.discard('red')
+# print(colors)                               ## {'blue123', 'grey', 'black', 'green'}
+# colors.clear()
+# print(colors)                               # set()
+
+# s = frozenset(colors) - не изменяемое множество
+
+
+########### СПИСКИ
+
+# list1=[1,12,3,4,5]
+# list2=list1
+#
+# list1[0] = 354
+# list2[1] = 777
+# for e in list1:
+#     print(e)
+#
+# print()
+#
+# for e in list2:
+#     print(e)
+
+
+list1 = [1,2,3,4,5]
+
+# print(len(list1))
+# print(list1.pop())
+# print(list1)  # [1, 2, 3, 4]
+# print(list1.pop(2))
+# print(list1)            # [1, 2, 4, 5]
+# print(list1.insert(2,11))
+# print(list1)                # [1, 2, 11, 3, 4, 5]
+# print(list1.append(97))
+# print(list1)                # [1, 2, 11, 3, 4, 5, 97]
